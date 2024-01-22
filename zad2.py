08b94cbbeef4bcd466b3f7ca4e3efa484683150a
Natalia Staniewska <nataliastaniewska4@gmail.com>
2024-01-22 14:41:00 +0100

    zad2

--- zad2.py
+++ zad2.py
@@ -21,4 +21,3 @@ if __name__ == "__main__":
     print(f"Odejmowanie: {subtract_numbers(num1,num2)}")
     print(f"Mnożenie: {multiply_numbers(num1, num2)}")
     print(f"Dzielenie: {divide_numbers(num1, num2)}")
-#ertyujk
